d04f9b531d8e8a333174e9562de77099ba7bd0c6
Memecoat <memecoat@outlook.com>
2020-10-17 23:45:19 +0300

    initial

--- js/src.js
+++ js/src.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
 	$("#btn-rawtext, .menu-link").hover(function() {
-		$(this).css("background-color", "#1b1b79");
+		$(this).css("background-color", "#977BBD");
 	},function() {
-		$(this).css("background-color", "#000038");
+		$(this).css("background-color", "#5b4a71");
 	});
 
 	$("#fldLeft, #fldMiddleT, #fldRight, #fldMiddleB").hover(function() {
-		$(this).css("background-color", "#F0027E");
+		$(this).css("background-color", "#75CFFF");
 	}, function() {
 		$(this).css("background-color", "#fff");
 	});
